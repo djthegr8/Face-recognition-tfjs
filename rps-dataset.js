e6cd0b3eaa97554f279232631c8161cbc72fb57f
djthegr8 <44142719+djthegr8@users.noreply.github.com>
2020-05-01 23:24:15 +0530

    Updated files with coments uploaded.
    This is the final upload before posting

---
+++ rps-dataset.js
@@ -0,0 +1,36 @@
+class RPSDataset {
+  constructor() {
+    this.labels = []
+  }
+  // You can add an example to this dataset using the below function
+  addExample(example, label) {
+    if (this.xs == null) {
+      this.xs = tf.keep(example);
+      this.labels.push(label);
+    } else {
+      const oldX = this.xs;
+      this.xs = tf.keep(oldX.concat(example, 0));
+      this.labels.push(label);
+      oldX.dispose();
+    }
+  }
+  
+  // This function will encode the ground truth labels
+  encodeLabels(numClasses) {
+    for (var i = 0; i < this.labels.length; i++) {
+      if (this.ys == null) {
+        this.ys = tf.keep(tf.tidy(
+            () => {return tf.oneHot(
+                tf.tensor1d([this.labels[i]]).toInt(), numClasses)}));
+      } else {
+        const y = tf.tidy(
+            () => {return tf.oneHot(
+                tf.tensor1d([this.labels[i]]).toInt(), numClasses)});
+        const oldY = this.ys;
+        this.ys = tf.keep(oldY.concat(y, 0));
+        oldY.dispose();
+        y.dispose();
+      }
+    }
+  }
+}
